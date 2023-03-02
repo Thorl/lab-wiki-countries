0d6acce45b497f11fe330e524617739e8e078efb
Thor Löfgren <lofgrenthor@gmail.com>
2023-03-02 21:31:52 +0100

    Complete iteration 5 and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { CountriesList } from './components/CountriesList';
-import CountryDetails from './components/CountryDetails'
+import CountryDetails from './components/CountryDetails';
 import { Navbar } from './components/Navbar';
 import './App.css';
 import axios from 'axios';
@@ -11,22 +11,25 @@ import axios from 'axios';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  useEffect(()=>{
-     axios.get(' https://ih-countries-api.herokuapp.com/countries')
-     .then((result)=>{
-      setCountries(result.data)
-      console.log(result.data)
-     })
-  },[])
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((result) => {
+        setCountries(result.data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <div className="country-container">
-      { countries && <CountriesList countries={countries}/>}
-      <Routes>
-        {/* <Route path="/" element={<CountriesList countries={countries} />} /> */}
-        <Route path="/:id" element={<CountryDetails countries={countries}/>}/>
-      </Routes>
+        {countries && <CountriesList countries={countries} />}
+        <Routes>
+          <Route
+            path="/:id"
+            element={<CountryDetails countries={countries} />}
+          />
+        </Routes>
       </div>
     </div>
   );
--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,37 +1,75 @@
-import { useParams } from "react-router-dom";
-function CountryDetails({countries}){
-   const {id} = useParams()
-   const country = countries.find(country => {
-    return id === country.alpha3Code.toLowerCase()
-   })
-   console.log('ID is:', id)
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
-    const countryCode = country.alpha2Code.toLowerCase();
-   console.log(country)
-    return(
-        <div className="country-detail">
-            <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryCode}.png`}/> 
-            <h3>{country.name.official}</h3>
-            <div>
-                <p>Capital</p>
-                <p>{country.capital[0]}</p>
-            </div>
-            <div>
-                <p>Area</p>
-                <p>{country.area} km2</p>
-            </div>
-            <div>
-                <p>Borders</p>
-                {country.borders.map((code)=>{
-                    const returnedCountry = countries.find((country)=>{
-                        if (country.alpha3Code === code)
-                        return country
-                    })
-                    return <p key={Date.now()*Math.random()}> {returnedCountry.name.official} </p>
-                })}
-            </div>
+import { useEffect, useState } from 'react';
 
-        </div>
-    )
+import './CountryDetails.css';
+
+function CountryDetails({ countries }) {
+  const [country, setCountry] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const country = countries.find((country) => {
+      return id === country.alpha3Code.toLowerCase();
+    });
+
+    if (country) {
+      axios
+        .get(
+          `https://ih-countries-api.herokuapp.com/countries/${country.alpha3Code}`
+        )
+        .then((result) => {
+          const countryData = result.data;
+
+          setCountry(countryData);
+        });
+    }
+  }, [id, countries]);
+
+  return (
+    <div className="country-details">
+      {!country && <h3>No details found for this country!</h3>}
+
+      {country && (
+        <>
+          <img
+            src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+            alt="The country's flag"
+          />
+          <h3>{country.name.official}</h3>
+          <div className="capital">
+            <p>Capital</p>
+            <p>{country.capital[0]}</p>
+          </div>
+          <hr style={{ width: '100%' }} />
+          <div className="area">
+            <p>Area</p>
+            <p>{country.area} km2</p>
+          </div>
+          <hr style={{ width: '100%' }} />
+          <div className="borders">
+            <p>Borders</p>
+            <div className="borders-list">
+              {country.borders.map((code) => {
+                const returnedCountry = countries.find((country) => {
+                  return country.alpha3Code === code;
+                });
+
+                return (
+                  <Link
+                    key={Date.now() * Math.random()}
+                    to={`/${returnedCountry.alpha3Code.toLowerCase()}`}
+                  >
+                    <p>{returnedCountry.name.official}</p>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
-export default CountryDetails
+export default CountryDetails;
